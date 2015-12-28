6ac558db4d22a2c4023a6abb9431754f8a15df78
yuzho <yuzho@SHAWYUZHO00.citrite.net>
2015-12-28 12:12:23 +0800

    init the project pom

--- spring-mvc-study/src/main/java/com/citrix/mvcdemo/controller/HelloMvcController.java
+++ spring-mvc-study/src/main/java/com/citrix/mvcdemo/controller/HelloMvcController.java
@@ -1,5 +1,17 @@
 package com.citrix.mvcdemo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+@RequestMapping("/hello")
 public class HelloMvcController {
+    
+    @RequestMapping("/mvc")
+    public String helloMvc() {
+        
+        return "home";
+    }
 
 }
